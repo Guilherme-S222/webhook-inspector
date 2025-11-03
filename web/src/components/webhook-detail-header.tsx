7a62b8f1ce268d9129ea4f2d9c59d6b6a2887917
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2025-11-03 13:20:03 -0300

    feat: web structure with react/vite, tailwind, tanstack, radix

---
+++ web/src/components/webhook-detail-header.tsx
@@ -0,0 +1,25 @@
+import { Bagde } from "./ui/bagde";
+
+export function WebhookDetailHeader() {
+  return (
+    <div className="space-y-4 border-b border-zinc-700 p-6">
+      <div className="flex items-center gap-3">
+        <Bagde>POST</Bagde>
+        <span className="text-lg font-medium text-zinc-300">
+          /video/status
+        </span>
+      </div>
+      <div className='flex items-center gap-2'>
+        <div className='flex items-center gap-1 text-sm text-zinc-400'>
+          <span>From IP</span>
+          <span className='underline underline-offset-4'>123.292.128.23</span>
+        </div>
+        <span className='w-px h-4 bg-zinc-700'/>
+        <div className='flex items-center gap-1 text-sm text-zinc-400'>
+          <span>at</span>
+          <span>April 18th, 14pm</span>
+        </div>
+      </div>
+    </div>
+  )
+}
