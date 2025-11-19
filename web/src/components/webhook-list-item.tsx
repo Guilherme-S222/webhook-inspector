de3e8c0617e6eaecb5f631ad5a5104f6e43fe5a0
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2025-11-18 22:10:54 -0300

    Finishing the third lesson.

--- web/src/components/webhook-list-item.tsx
+++ web/src/components/webhook-list-item.tsx
@@ -3,23 +3,42 @@ import { IconButton } from './ui/icon-button'
 import { Trash2Icon } from 'lucide-react'
 import { Checkbox } from './ui/checkbox'
 import { formatDistanceToNow } from 'date-fns'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 interface WebhookListItemProps {
   webhook: {
-    id: string,
-    method: string,
-    pathname: string,
+    id: string
+    method: string
+    pathname: string
     createdAt: Date
   }
 }
 
 export function WebhookListItem({ webhook }: WebhookListItemProps) {
+  const queryClient = useQueryClient()
+  const { mutate: deleteWebhook } = useMutation({
+    mutationFn: async (id: string) => {
+      await fetch(`http://localhost:3333/api/webhooks/${id}`, {
+        method: 'DELETE',
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['webhooks'],
+      })
+    },
+  })
+
   return (
     <div className="group rounde-lg transition-colors duration-150 hover:bg-zinc-700/30">
       <div className="flex items-start gap-3 px-4 py-2.5">
         <Checkbox />
 
-        <Link to="/webhooks/$id" params={{ id: webhook.id }} className="flex flex-1 min-w-0 items-start gap-3">
+        <Link
+          to="/webhooks/$id"
+          params={{ id: webhook.id }}
+          className="flex flex-1 min-w-0 items-start gap-3"
+        >
           <span className="w-12 shrink-0 font-mono text-xs font-semibold text-zinc-300 text-right">
             {webhook.method}
           </span>
@@ -35,6 +54,7 @@ export function WebhookListItem({ webhook }: WebhookListItemProps) {
         <IconButton
           icon={<Trash2Icon className="size-3.5 text-zinc-400" />}
           className="opacity-0 transition-opacity group-hover:opacity-100"
+          onClick={() => deleteWebhook(webhook.id)}
         />
       </div>
     </div>
